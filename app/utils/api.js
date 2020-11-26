2210cb70b55275a4062acd36f1b1b2b41b10424b
Giovanni Martinez <curare.infantem@gmail.com>
2020-11-26 00:03:18 -0600

    Cambio a servidor de testing

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,7 @@
 // Add https to instance (port 443)
 
 // Prod
-// export const API = "http://3.16.220.202:3001";
+export const API = "http://3.16.220.202:3001";
 
 // Dev
-export const API = "http://192.168.100.5:3001";
+// export const API = "http://192.168.100.5:3001";
